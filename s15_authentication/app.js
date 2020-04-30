02906a8217efbc353dfc1c9bef9136f2b5aab706
Emily Dela Cruz <delacruz.emily@gmail.com>
2020-04-30 13:53:02 -0400

    s15 auth middleware

--- s15_authentication/app.js
+++ s15_authentication/app.js
@@ -58,18 +58,6 @@ app.use(errorController.get404);
 mongoose
   .connect(MONGODB_URI)
   .then(result => {
-    User.findOne().then(user => {
-      if (!user) {
-        const user = new User({
-          name: "Ema",
-          email: "ema@test.com",
-          cart: {
-            items: []
-          }
-        });
-        user.save();
-      }
-    });
     app.listen(3000);
   })
   .catch(err => {
--- s15_authentication/models/user.js
+++ s15_authentication/models/user.js
@@ -1,13 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  name: {
+  email: {
     type: String,
     required: true
   },
-  email: {
+  password: {
     type: String,
     required: true
   },
@@ -16,7 +16,7 @@ const userSchema = new Schema({
       {
         productId: {
           type: Schema.Types.ObjectId,
-          ref: 'Product',
+          ref: "Product",
           required: true
         },
         quantity: { type: Number, required: true }
@@ -61,7 +61,7 @@ userSchema.methods.clearCart = function() {
   return this.save();
 };
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
 
 // const mongodb = require('mongodb');
 // const getDb = require('../util/database').getDb;
